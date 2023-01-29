f20e04a2ff9a4219fe7b1b8b2b27d57c29238d2a
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-01-29 19:52:39 +0900

    csvの読み取り

--- FlagQuizApp/ViewController.swift
+++ FlagQuizApp/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// グローバル変数宣言
+var countries: [County] = []
 
+class ViewController: UIViewController {
+    
+    var csvArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //
+        
+        //
     }
 
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
+    }
 
 }
 
