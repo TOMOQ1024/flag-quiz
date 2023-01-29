93d27b4ee9815fda8af7cdde43a54de218d6f10f
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-01-29 15:34:31 +0900

    add questions view controller

---
+++ FlagQuizApp/QuestionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionsViewController.swift
+//  FlagQuizApp
+//
+//  Created by 鈴木智久 on 2023/01/29.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
